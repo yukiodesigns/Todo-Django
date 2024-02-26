3a51d6f949f853e8a43a7df5486300b855479a79
yuki designs <yukiosdesigns@gmail.com>
2024-02-26 23:18:41 +0300

    Created model

--- todo/settings.py
+++ todo/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todoApi'
+    'todoApi',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
