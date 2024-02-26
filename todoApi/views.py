0b99bcc7fa17991b0f671512512d1220fd312b6c
yuki designs <yukiosdesigns@gmail.com>
2024-02-26 23:53:24 +0300

    Added single task view

--- todoApi/views.py
+++ todoApi/views.py
@@ -21,3 +21,9 @@ def taskList(request):
     tasks = Task.objects.all()
     serializer = TaskSerializer(tasks, many = True)
     return Response(serializer.data)
+
+@api_view(['GET'])
+def taskDetail(request, pk):
+    tasks = Task.objects.get(id=pk)
+    serializer = TaskSerializer(tasks, many = False)
+    return Response(serializer.data)
