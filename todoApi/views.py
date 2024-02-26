bb15044c5a279b9be05014d15fb62c039b4207ca
yuki designs <yukiosdesigns@gmail.com>
2024-02-26 23:50:42 +0300

    Added serializer, urls and vies

--- todoApi/views.py
+++ todoApi/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializer import TaskSerializer
+from .models import Task
+"""
+API Overview
+"""
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {
+        'List' : '/task-list/',
+        'Detail View' : '/task-detail/<str:pk>/',
+        'Create' : '/task-create/',
+        'Update' : '/task-update/<str:pk>/',
+        'Delete' : '/task-delete/<str:pk>/',
+    }
+    return Response(api_urls)
 
-# Create your views here.
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many = True)
+    return Response(serializer.data)
